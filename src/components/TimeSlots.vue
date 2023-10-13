91e0e7e592a7e9b6bc134b085c403b7acb581cae
Igor Moraes <igorfm13524@gmail.com>
2023-10-12 22:42:30 -0300

    Adiciona todos os intervalos de horários

--- src/components/TimeSlots.vue
+++ src/components/TimeSlots.vue
@@ -76,33 +76,123 @@
 		available_slots = ref({
 			1: {
 				id: 1,
-				start: '9:30',
-				finish: '10:00',
+				start: '7:00',
+				finish: '7:30',
 			},
 			2: {
 				id: 2,
-				start: '10:00',
-				finish: '10:30',
+				start: '7:30',
+				finish: '8:00',
 			},
 			3: {
 				id: 3,
-				start: '10:30',
-				finish: '11:00',
+				start: '8:00',
+				finish: '8:30',
 			},
 			4: {
 				id: 4,
-				start: '11:00',
-				finish: '11:30',
+				start: '8:30',
+				finish: '9:00',
 			},
 			5: {
 				id: 5,
-				start: '11:30',
-				finish: '12:00',
+				start: '9:00',
+				finish: '9:30',
 			},
 			6: {
 				id: 6,
+				start: '9:30',
+				finish: '10:00',
+			},
+			7: {
+				id: 7,
+				start: '10:00',
+				finish: '10:30',
+			},
+			8: {
+				id: 8,
+				start: '10:30',
+				finish: '11:00',
+			},
+			9: {
+				id: 9,
+				start: '11:00',
+				finish: '11:30',
+			},
+			10: {
+				id: 10,
+				start: '11:30',
+				finish: '12:00',
+			},
+			11: {
+				id: 11,
 				start: '12:00',
 				finish: '12:30',
+			},
+			12: {
+				id: 12,
+				start: '12:30',
+				finish: '13:00',
+			},
+			13: {
+				id: 13,
+				start: '13:00',
+				finish: '13:30',
+			},
+			14: {
+				id: 14,
+				start: '13:30',
+				finish: '14:00',
+			},
+			15: {
+				id: 15,
+				start: '14:00',
+				finish: '14:30',
+			},
+			16: {
+				id: 16,
+				start: '14:30',
+				finish: '15:00',
+			},
+			17: {
+				id: 17,
+				start: '15:00',
+				finish: '15:30',
+			},
+			18: {
+				id: 18,
+				start: '15:30',
+				finish: '16:00',
+			},
+			19: {
+				id: 19,
+				start: '16:00',
+				finish: '16:30',
+			},
+			20: {
+				id: 20,
+				start: '16:30',
+				finish: '17:00',
+			},
+			21: {
+				id: 21,
+				start: '17:00',
+				finish: '17:30',
+			},
+			22: {
+				id: 22,
+				start: '17:30',
+				finish: '18:00',
+			},
+			23: {
+				id: 23,
+				start: '18:00',
+				finish: '18:30',
+			},
+			24: {
+				id: 24,
+				start: '18:30',
+				finish: '19:00',
 			},
 		});
 		selected_day_doc = await getDoc(
