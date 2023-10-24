208ca3350b808c19959752edde0dbcc816d2c7b4
Igor Moraes <igorfm13524@gmail.com>
2023-10-23 21:53:59 -0300

    Mostra todos os agendamentos

--- src/views/AllDates.vue
+++ src/views/AllDates.vue
@@ -1,8 +1,27 @@
 <template>
 	<ion-page>
-		<ion-content> I'm an Admin </ion-content>
+		<ion-content>
+			<p v-for="date in dates">
+				{{ date }}
+			</p>
+		</ion-content>
 	</ion-page>
 </template>
 <script setup>
 	import { IonPage, IonContent } from '@ionic/vue';
+	import { ref } from 'vue';
+	import { collection, onSnapshot } from 'firebase/firestore';
+	import { database } from '../firebase.js';
+
+	const dates = ref([]);
+
+	const dates_snapshot = onSnapshot(
+		collection(database, 'appointments'),
+		(col) => {
+			dates.value.length = 0;
+			col.forEach((doc) => {
+				dates.value.push(doc.data());
+			});
+		}
+	);
 </script>
