c95e3b0b7572ce1585a3f27691e6ce00e959577e
Igor Moraes <igorfm13524@gmail.com>
2023-11-06 18:16:57 -0300

    Adiciona novos serviços

--- src/modules/bussiness_reference.js
+++ src/modules/bussiness_reference.js
@@ -1,174 +1,3 @@
-export const slots_reference = new Map([
-	[
-		1,
-		{
-			start: '7:00',
-			finish: '7:30',
-		},
-	],
-	[
-		2,
-		{
-			start: '7:30',
-			finish: '8:00',
-		},
-	],
-	[
-		3,
-		{
-			start: '8:00',
-			finish: '8:30',
-		},
-	],
-	[
-		4,
-		{
-			start: '8:30',
-			finish: '9:00',
-		},
-	],
-	[
-		5,
-		{
-			start: '9:00',
-			finish: '9:30',
-		},
-	],
-	[
-		6,
-		{
-			start: '9:30',
-			finish: '10:00',
-		},
-	],
-	[
-		7,
-		{
-			start: '10:00',
-			finish: '10:30',
-		},
-	],
-	[
-		8,
-		{
-			start: '10:30',
-			finish: '11:00',
-		},
-	],
-	[
-		9,
-		{
-			start: '11:00',
-			finish: '11:30',
-		},
-	],
-	[
-		10,
-		{
-			start: '11:30',
-			finish: '12:00',
-		},
-	],
-	[
-		11,
-		{
-			start: '12:00',
-			finish: '12:30',
-		},
-	],
-	[
-		12,
-		{
-			start: '12:30',
-			finish: '13:00',
-		},
-	],
-	[
-		13,
-		{
-			start: '13:00',
-			finish: '13:30',
-		},
-	],
-	[
-		14,
-		{
-			start: '13:30',
-			finish: '14:00',
-		},
-	],
-	[
-		15,
-		{
-			start: '14:00',
-			finish: '14:30',
-		},
-	],
-	[
-		16,
-		{
-			start: '14:30',
-			finish: '15:00',
-		},
-	],
-	[
-		17,
-		{
-			start: '15:00',
-			finish: '15:30',
-		},
-	],
-	[
-		18,
-		{
-			start: '15:30',
-			finish: '16:00',
-		},
-	],
-	[
-		19,
-		{
-			start: '16:00',
-			finish: '16:30',
-		},
-	],
-	[
-		20,
-		{
-			start: '16:30',
-			finish: '17:00',
-		},
-	],
-	[
-		21,
-		{
-			start: '17:00',
-			finish: '17:30',
-		},
-	],
-	[
-		22,
-		{
-			start: '17:30',
-			finish: '18:00',
-		},
-	],
-	[
-		23,
-		{
-			start: '18:00',
-			finish: '18:30',
-		},
-	],
-	[
-		24,
-		{
-			start: '18:30',
-			finish: '19:00',
-		},
-	],
-]);
-
 export const services_reference = 2;
 export const staff_reference = {
 	Igor: {
@@ -188,7 +17,23 @@ export const staff_reference = {
 			{ name: 'Pé', duration: 40 },
 			{ name: 'Plástica nos Pés', duration: 35 },
 			{ name: 'Escalada Pés', duration: 40 },
+			{ name: 'Buço Depilação Cera', duration: 15 },
+			{ name: 'Axila Depilação Cera', duration: 30 },
+			{ name: 'Perna Inteira Depilação Cera', duration: 60 },
+			{ name: 'Meia Perna Depilação Cera', duration: 30 },
+			{ name: 'Virilha Depilação Cera', duration: 50 },
+			{ name: 'Massagem Relaxante', duration: 60 },
+			{ name: 'Hidra Gloos', duration: 60 },
 		],
 		id: 2,
 	},
+    Mayara: {
+        name: 'Mayara',
+        services: [ 
+			{ name: 'Aplicação Extensão de Cílios', duration: 180 },
+			{ name: 'Manutenção Extensão de Cílios', duration: 120 },
+			{ name: 'Remoção Extensão de Cílios', duration: 60 },
+        ],
+        id: 3,
+    }
 };
