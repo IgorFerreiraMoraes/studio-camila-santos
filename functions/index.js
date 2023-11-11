8b12b9e429c2fa77bd41b09641ef1136cff2d8e5
Wandering Raven <igorfm13524@gmail.com>
2023-11-11 17:20:02 -0300

    mend

--- functions/index.js
+++ functions/index.js
@@ -7,35 +7,13 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const { logger } = require('firebase-functions');
-const { onSchedule } = require('firebase-functions/v2/schedules');
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-// The Firebase Admin SDK to access Firestore.
-const admin = require('firebase-admin');
-const { event } = require('firebase-functions/v1/analytics');
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-admin.initializeApp();
-
-exports.delete_past_appointments = onSchedule(
-	'every day 19:00',
-	async (event) => {
-		const now = new Date();
-
-		const appointments = admin.firestore().collection('appointments');
-		const past_appointments_query = appointments.where(
-			'end_time',
-			'<',
-			now
-		);
-		const past_appointments = await past_appointments_query.get();
-
-		const delete_promises = [];
-		past_appointments.forEach((doc) => {
-			delete_promises.push(doc.ref.delete());
-		});
-
-		await Promise.all(delete_promises);
-		return null;
-	}
-);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
