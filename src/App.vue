5dfa0e61fdd2a4637653686bf00efb9486aaa732
Wandering Raven <igorfm13524@gmail.com>
2023-11-22 14:31:41 -0300

    Lida com o evento `onMessage` no arquivo principal

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,13 @@
 
 <script setup>
 	import { IonApp, IonRouterOutlet } from '@ionic/vue';
-
+	import { getMessaging, onMessage } from 'firebase/messaging';
 	import { get_and_write_user_messaging_token } from './modules/messaging_token';
+
 	get_and_write_user_messaging_token();
+
+	const messaging = getMessaging();
+	onMessage(messaging, (payload) => {
+		console.log('Message received. ', payload);
+	});
 </script>
