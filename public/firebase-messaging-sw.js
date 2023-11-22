54271b5a3f72f0e5cad6410a00163e04b36cd4ac
Wandering Raven <igorfm13524@gmail.com>
2023-11-22 15:16:55 -0300

    Remove configurações desnecessárias para notificação

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -22,15 +22,5 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
-	console.log(
-		'[firebase-messaging-sw.js] Received background message ',
-		payload
-	);
-	// Customize notification here
-	const notificationTitle = 'Background Message Title';
-	const notificationOptions = {
-		body: 'Background Message body.',
-	};
-
-	self.registration.showNotification(notificationTitle, notificationOptions);
+	return payload;
 });
