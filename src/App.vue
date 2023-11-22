33fc82d583270d737af79fd431ca0a5445b222cb
Wandering Raven <igorfm13524@gmail.com>
2023-11-22 14:30:25 -0300

    Checa token de mensagem assim que o app carrega

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,7 @@
 
 <script setup>
 	import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+	import { get_and_write_user_messaging_token } from './modules/messaging_token';
+	get_and_write_user_messaging_token();
 </script>
