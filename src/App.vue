ed7f6e6795b5a4617e2b1e5b7e33b299df9bfdab
Wandering Raven <igorfm13524@gmail.com>
2023-12-07 11:25:03 -0300

    :sparkles: feat: Modal para instalar

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,52 @@
 </template>
 
 <script setup>
-    import { onMounted } from 'vue';
-    import { IonApp, IonRouterOutlet } from '@ionic/vue';
+    import { onMounted, ref } from 'vue';
+    import {
+        IonApp,
+        IonRouterOutlet,
+        modalController,
+    } from '@ionic/vue';
     import { getMessaging, onMessage } from 'firebase/messaging';
     import { check_user_messaging_token } from './modules/messaging_token';
+    import InstallModal from './components/InstallModal.vue';
+
+    let prompt;
+    const is_modal_open = ref(false);
 
     onMounted(() => {
+        check_installation();
         setup_message_handling();
     });
+
+    async function check_installation() {
+        if (!user_in_browser()) return;
+
+        window.addEventListener('beforeinstallprompt', (event) => {
+            event.preventDefault();
+            prompt = event;
+
+            if (!is_modal_open.value) {
+                show_install_modal(prompt);
+                is_modal_open.value = true;
+            }
+        });
+    }
+
+    function user_in_browser() {
+        return (
+            typeof window !== 'undefined' &&
+            'BeforeInstallPromptEvent' in window
+        );
+    }
+
+    async function show_install_modal(prompt) {
+        const modal = await modalController.create({
+            component: InstallModal,
+            componentProps: { browser_prompt: prompt },
+        });
+        modal.present();
+    }
 
     function setup_message_handling() {
         check_user_messaging_token();
---
+++ src/components/InstallModal.vue
@@ -0,0 +1,56 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-buttons slot="start">
+                <ion-button
+                    color="medium"
+                    @click="cancel"
+                >
+                    Cancelar
+                </ion-button>
+            </ion-buttons>
+            <ion-title>Instale Nosso Aplicativo</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        <ion-item>
+            <ion-label class="ion-text-wrap">
+                Gostaria de um acesso rápido e fácil aos seus
+                agendamentos? Clique no botão abaixo para instalar o
+                aplicativo!
+            </ion-label>
+        </ion-item>
+        <ion-button
+            @click="show_install_prompt()"
+            color="tertiary"
+            expand="block"
+        >
+            Adicionar à Tela Inicial
+        </ion-button>
+    </ion-content>
+</template>
+
+<script setup>
+    import {
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonButtons,
+        IonButton,
+        IonItem,
+        IonLabel,
+        modalController,
+    } from '@ionic/vue';
+
+    const props = defineProps(['browser_prompt']);
+    console.log(props.browser_prompt);
+
+    function show_install_prompt() {
+        props.browser_prompt.prompt();
+    }
+
+    function cancel() {
+        modalController.dismiss(null, 'cancel');
+    }
+</script>
