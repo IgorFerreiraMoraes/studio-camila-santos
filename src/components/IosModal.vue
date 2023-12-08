39f485d4778707d0e901a90a1f75dfe0004ba597
Wandering Raven <igorfm13524@gmail.com>
2023-12-08 15:41:01 -0300

    :sparkles: feat: Cria Modal para iOS

---
+++ src/components/IosModal.vue
@@ -0,0 +1,56 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-buttons slot="start">
+                <ion-button
+                    color="medium"
+                    @click="cancel"
+                >
+                    Ok
+                </ion-button>
+            </ion-buttons>
+            <ion-title>Aplicativo</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        <ion-item>
+            <ion-label class="ion-text-wrap">
+                Instale este aplicativo em seu iPhone: aperte em
+                <ion-icon
+                    :icon="shareOutline"
+                    color="tertiary"
+                ></ion-icon>
+                e depois em 'Adicionar à Tela de Início'
+            </ion-label>
+        </ion-item>
+    </ion-content>
+</template>
+
+<script setup>
+    import {
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonButtons,
+        IonButton,
+        IonItem,
+        IonLabel,
+        IonIcon,
+        modalController,
+    } from '@ionic/vue';
+    import { shareOutline } from 'ionicons/icons';
+
+    function cancel() {
+        modalController.dismiss(null, 'cancel');
+    }
+</script>
+<style scoped>
+    .ion-text-wrap {
+        font-size: 1.1rem;
+    }
+    ion-icon {
+        width: 1.5rem;
+        height: 1.5rem;
+    }
+</style>
