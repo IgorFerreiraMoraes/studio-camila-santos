0b95463063af9612859595504f6880849349cb6e
Wandering Raven <igorfm13524@gmail.com>
2023-12-08 15:41:55 -0300

    :sparkles: feat: Cria Modal caso não seja possível instalar

---
+++ src/components/AlternativesModal.vue
@@ -0,0 +1,66 @@
+<template>
+    <ion-header>
+        <ion-toolbar>
+            <ion-title>Aplicativo</ion-title>
+        </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+        <ion-item>
+            <ion-label class="ion-text-wrap">
+                Olá! No momento, não conseguimos verificar se seu
+                navegador suporta a instalação de aplicativos web. Aqui
+                estão algumas dicas:
+
+                <br />
+                <br />
+
+                1. Procure por 'Instalar aplicativo' ou 'Adicionar à
+                tela inicial' no menu do seu navegador, (geralmente
+                <ion-icon
+                    :icon="ellipsisVertical"
+                    color="tertiary"
+                ></ion-icon>
+                ou similares).
+
+                <br />
+                <br />
+
+                2. Se não encontrar, considere instalar o Google Chrome,
+                que costuma oferecer melhor compatibilidade.
+
+                <br />
+                <br />
+
+                3. Caso prefira, continue usando o aplicativo no
+                navegador. Todos os recursos estão disponíveis!
+            </ion-label>
+        </ion-item>
+        <ion-button
+            @click="cancel"
+            class="ion-margin-horizontal"
+            color="tertiary"
+            expand="block"
+        >
+            Continuar por Aqui
+        </ion-button>
+    </ion-content>
+</template>
+
+<script setup>
+    import {
+        IonContent,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonButton,
+        IonItem,
+        IonLabel,
+        IonIcon,
+        modalController,
+    } from '@ionic/vue';
+    import { ellipsisVertical } from 'ionicons/icons';
+
+    function cancel() {
+        modalController.dismiss(null, 'cancel');
+    }
+</script>
