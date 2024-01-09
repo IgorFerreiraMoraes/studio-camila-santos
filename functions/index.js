670292d6e004b3337bc6e46e81e641ebff1aba05
Wandering Raven <igorfm13524@gmail.com>
2024-01-09 01:46:07 -0300

    :sparkles: feat: Mensagem de lembrete no dia do agendament

--- functions/index.js
+++ functions/index.js
@@ -65,6 +65,52 @@ exports.send_birthday_message = onSchedule(
     },
 );
 
+exports.send_reminder_message = onSchedule(
+    'every day 08:00',
+    async (event) => {
+        const today = new Date(
+            new Date().toLocaleString({
+                timeZone: 'America/Sao_Paulo',
+            }),
+        );
+        today.setHours(0, 0, 0, 0);
+
+        const appointments = admin
+            .firestore()
+            .collection('appointments');
+        const today_appointments_query = appointments.where(
+            'date',
+            '==',
+            today.toString(),
+        );
+        const today_appointments = await today_appointments_query.get();
+
+        today_appointments.forEach(async (doc) => {
+            const appointment_time = doc.data().start_time;
+            const appointment_hour = `${appointment_time.getHours()}:${appointment_time.getMinutes()}`;
+
+            const client_id = doc.data().client_id;
+            const client_reference = admin
+                .firestore()
+                .collection('users')
+                .doc(client_id);
+            const client_snap = await client_reference.get();
+            const client = client_snap.data();
+
+            const message = {
+                notification: {
+                    title: `${client.name}, seu horário é hoje!`,
+                    body: `Às ${appointment_hour} com ${
+                        doc.data().staff_name
+                    }. Mal podemos esperar para vê-la! Até breve!`,
+                },
+                token: client.messaging_token,
+            };
+            admin.messaging().send(message);
+        });
+    },
+);
+
 exports.set_admin = onCall(async (request) => {
     const new_admin_email = request.data.email;
     let new_admin_uid;
